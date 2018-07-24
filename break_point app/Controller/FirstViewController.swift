c3ad394a085da69bf45ffb01eaecddea8684d595
takhmina0907 <lollis0907@gmail.com>
2018-07-24 23:18:12 +0600

    first commit

---
+++ break_point app/Controller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  break_point app
+//
+//  Created by Takhmina Talipova on 7/24/18.
+//  Copyright © 2018 Takhmina Talipova. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
